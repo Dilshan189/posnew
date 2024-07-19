6649803c4f9d3085268cb0535a1fed022868bf39
Dilshan189 <142502900+Dilshan189@users.noreply.github.com>
2024-07-19 23:59:20 +0530

    first commit

---
+++ src/superpos/DBConnection.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superpos;
+import java.sql.*;
+
+/**
+ *
+ * @author DELL
+ */
+/*public class DBConnection {
+    
+    static final String DB_URL = "jdbc.mysql://localhost/superpos";
+    static final String USER = "root";
+    static final String PASS = "";
+    public static Connection connectDB(){
+        Connection conn = null;
+        try {
+           Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(DB_URL,USER,PASS);
+            return conn;
+        }catch(Exception ex) {
+           System.out.println("This were errors db.");
+            return null;
+        }
+    }
+}
+*/
